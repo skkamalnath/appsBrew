bfd1c9305106e2b63428086ed71260deb35440c9
kamalnath <sk.kamalnath@gmail.com>
2017-04-26 09:04:48 +0530

    Delete about.component.ts

--- src/app/about/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: 'about-us.component.html',
-  styleUrls: ['./about-us.component.css'],
-})
-
-export class AboutUsComponent { }
